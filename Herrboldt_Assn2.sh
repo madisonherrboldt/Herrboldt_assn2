846cc1d70fe275818f296650ad5685a9fe8d7dbc
Madison Herrboldt <mah1626@utulsa.edu>
2020-09-22 15:04:57 -0500

    q  Untracked files:
    esc ZZ
    escZZ
    
    esc :wq return

--- Herrboldt_Assn2.sh
+++ Herrboldt_Assn2.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
 
+##create child script
 touch Herrboldt_child_script.sh
+
+##give permissions to run child script
+chmod u+x Herrboldt_child_script.sh
+
+##add shebang to child script
+echo "#! /bin/bash" >> Herrboldt_child_script.sh
+
+##cut and extract columns 1 to 3 (ID Year and Trappability)
+echo "cut -f 1-2,4 BTS_data.txt > Herrboldt_output1.txt" >> Herrboldt_child_script.sh
+
+##steal the heading from output 1 and transfer to new output file
+
