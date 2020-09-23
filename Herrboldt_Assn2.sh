1933232500915f3dfbaa98ec3b0d29316f7971e6
Madison Herrboldt <mah1626@utulsa.edu>
2020-09-23 11:24:01 -0500

    almost done

--- Herrboldt_Assn2.sh
+++ Herrboldt_Assn2.sh
@@ -13,4 +13,14 @@ echo "#! /bin/bash" >> Herrboldt_child_script.sh
 echo "cut -f 1-2,4 BTS_data.txt > Herrboldt_output1.txt" >> Herrboldt_child_script.sh
 
 ##steal the heading from output 1 and transfer to new output file
+echo "head -n1 Herrboldt_output1.txt > Herrboldt_output.txt" >> Herrboldt_child_script.sh
 
+##sort output 1 by trappability in reverse order
+echo "sort -k3rg Herrboldt_output1.txt > Herrboldt_output2.txt" >> Herrboldt_child_script.sh
+
+##get sequence of snakes 13BTS010-13BTS089
+echo "grep BTS0[1-8] Herrboldt_output2.txt >> Herrboldt_output.txt" >> Herrboldt_child_script.sh
+
+##remove the unwanted intermediate output files
+echo "rm Herrboldt_output1.txt" >> Herrboldt_child_script.sh
+echo "rm Herrboldt_output2.txt" >> Herrboldt_child_script.sh
